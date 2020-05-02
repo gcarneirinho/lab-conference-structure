bb6833bd6fa7c461610925d5a30fde8897c3a2a4
Gustavo <carneirinho@gmail.com>
2020-05-02 15:08:50 -0300

    Added more buttons and toggle

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,13 @@ $(function () {
     $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
   });
 
+  $('#btn-copiar').click(function (event){
+    event.preventDefault();
+    $(this).toggleClass('green');
+
+    var text = $('#btn-copiar').text();
+    $('#btn-copiar').text(text == "Copiar" ? "Copiado" : "Copiar");
+  })
+
 });
 
